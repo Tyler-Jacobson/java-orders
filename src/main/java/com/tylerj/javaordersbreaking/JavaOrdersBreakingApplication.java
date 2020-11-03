80529c4e6e8b2137cba98fa9bbe652f32e412ef1
Tyler Jacobson <TylerJacobsonSE@gmail.com>
2020-11-02 17:40:51 -0800

    Test Commit

---
+++ src/main/java/com/tylerj/javaordersbreaking/JavaOrdersBreakingApplication.java
@@ -0,0 +1,13 @@
+package com.tylerj.javaordersbreaking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOrdersBreakingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaOrdersBreakingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tylerj/javaordersbreaking/JavaOrdersBreakingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tylerj.javaordersbreaking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaOrdersBreakingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
